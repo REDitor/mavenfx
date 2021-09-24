1a026ae8e78790be41cc53d2c5f8c96b5877fd61
sanderharks <650397@student.inholland.nl>
2021-09-24 18:07:41 +0200

    Almost done

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -1,6 +1,9 @@
 package nl.inholland.javafx;
 
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,16 +19,27 @@ public class App extends Application {
         window.setHeight(200);
         window.setWidth(300);
         window.setTitle("Week 4 - Lesson 2 Exercise - Login Screen");
+        loadWindow(window);
+    }
 
-        //Basic Structure
+    private void loadWindow(Stage window) {
         GridPane gridPane = new GridPane();
         gridPane.setPadding(new Insets(10, 10, 10, 10));
         gridPane.setVgap(10); // Vertical spacing between grid items
         gridPane.setHgap(8); // Horizontal spacing between grid items
+        addGridPaneElements(window, gridPane);
 
+        //Display UI elements (the gridpane) in scene + display window
+        Scene scene = new Scene(gridPane);
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void addGridPaneElements(Stage window, GridPane gridPane) {
         //Elements
         Label lblUsername = new Label("Username");
         Label lblPassword = new Label("Password");
+        Label lblVisiblePassword = new Label();
 
         TextField txtUsername = new TextField();
         txtUsername.setPromptText("Enter username");
@@ -33,7 +47,25 @@ public class App extends Application {
         PasswordField pfPassword = new PasswordField();
         pfPassword.setPromptText("Enter password");
 
+        //Store info/data
+        String username = txtUsername.getText();
+        String password = pfPassword.getText();
+
+        StringProperty pfPasswordProperty = pfPassword.textProperty();
+        lblVisiblePassword.textProperty().bind(pfPasswordProperty);
+
         Button btnLogin = new Button("Log in");
+        btnLogin.setVisible(false);
+
+        //FIXME: Button not appearing on met criteria
+        pfPasswordProperty.addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                if (password.matches("^(?=.*[0-9])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8}$")) {
+                    btnLogin.setVisible(true);
+                }
+            }
+        });
 
         //Grid Assignment
         GridPane.setConstraints(lblUsername, 0, 0);
@@ -41,15 +73,7 @@ public class App extends Application {
         GridPane.setConstraints(txtUsername, 1, 0);
         GridPane.setConstraints(pfPassword, 1, 1);
         GridPane.setConstraints(btnLogin, 1, 2);
-        gridPane.getChildren().addAll(lblUsername, lblPassword, txtUsername, pfPassword, btnLogin);
-
-        //Display UI elements (the gridpane) in scene + display window
-        Scene scene = new Scene(gridPane);
-        window.setScene(scene);
-        window.show();
-
-        //Store info/data (not used right now)
-        String username = txtUsername.getText();
-        String password = pfPassword.getText();
+        GridPane.setConstraints(lblVisiblePassword, 0, 4);
+        gridPane.getChildren().addAll(lblUsername, lblPassword, txtUsername, pfPassword, btnLogin, lblVisiblePassword);
     }
 }
