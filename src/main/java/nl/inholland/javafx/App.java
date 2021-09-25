869a1b9db953a5d3b6641a1c7c110925f41f95ea
sanderharks <650397@student.inholland.nl>
2021-09-25 14:52:16 +0200

    Start of TICTACTOE

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -1,52 +1,44 @@
 package nl.inholland.javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
-    //I don't know if this is the right way to
-    //be able to access the elements but this way
-    //I can use them in every method.
-
-    Label lblEuroAmount = new Label("Amount €:");
-    TextField txtEuroAmount = new TextField();
-    Button btnConvertToDollar = new Button("Convert Euro to Dollar");
-    Label lblDollarAmount = new Label("Amount $:");
-    Label lblDollarAmountResult = new Label("...");
+    Button btnUpperLeft = new Button("_");
+    Button btnUpperMid = new Button("_");
+    Button btnUpperRight = new Button("_");
+    Button btnMiddleLeft = new Button("_");
+    Button btnMiddleMid = new Button("_");
+    Button btnMiddleRight = new Button("_");
+    Button btnLowerLeft = new Button("_");
+    Button btnLowerMid = new Button("_");
+    Button btnLowerRight = new Button("_");
+    Label lblCurrentTurn = new Label("Current turn:");
+    Label lblCurrentTurnResult = new Label();
     GridPane gridPane = new GridPane();
 
     @Override
     public void start(Stage window) throws Exception {
         loadWindow(window);
-        btnConvertToDollar.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                double euro = Double.parseDouble(txtEuroAmount.getText());
-                double dollarResult = convertEuroToDollar(euro);
-                lblDollarAmountResult.setText(String.format("%.2f", convertEuroToDollar(euro)));
-            }
-        });
+
     }
 
     //Methods
     private void loadWindow(Stage window) {
-        window.setHeight(200);
-        window.setWidth(300);
+        window.setHeight(300);
+        window.setWidth(400);
         window.setTitle("Currency Converter");
         window.setX(100);
 
         gridPane.setPadding(new Insets(10, 10, 10, 10));
         gridPane.setVgap(10);
-        gridPane.setHgap(8);
+        gridPane.setHgap(10);
         assignGrid();
         Scene scene = new Scene(gridPane);
         window.setScene(scene);
@@ -54,15 +46,18 @@ public class App extends Application {
     }
 
     private void assignGrid() {
-        GridPane.setConstraints(lblEuroAmount, 0, 0);
-        GridPane.setConstraints(txtEuroAmount, 1, 0);
-        GridPane.setConstraints(btnConvertToDollar, 1, 1);
-        GridPane.setConstraints(lblDollarAmount, 0, 2);
-        GridPane.setConstraints(lblDollarAmountResult, 1, 2);
-        gridPane.getChildren().addAll(lblEuroAmount, txtEuroAmount, btnConvertToDollar, lblDollarAmount, lblDollarAmountResult);
-    }
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
+        GridPane.setConstraints(, 0, 0);
 
-    public double convertEuroToDollar(double euro) {
-        return euro * 1.18;
+        gridPane.getChildren().addAll(lblEuroAmount, txtEuroAmount, btnConvertToDollar, lblDollarAmount, lblDollarAmountResult);
     }
 }
