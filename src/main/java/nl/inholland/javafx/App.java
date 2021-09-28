74b0bc0d34d2c77db12208b5e571f5ec877be6d4
REDitor <71357600+REDitor@users.noreply.github.com>
2021-09-28 17:12:47 +0200

    Applied some structure

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,6 +14,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class App extends Application {
     //region elements
@@ -24,25 +28,11 @@ public class App extends Application {
     GridPane gridPane = new GridPane();
     //endregion
 
-    @Override
-    public void start(Stage window) throws Exception {
-        loadWindow(window);
-
-        StringProperty pfPasswordProperty = pfPassword.textProperty();
-        pfPasswordProperty.addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                btnLogin.setVisible(lblVisiblePassword.getText().length() >= 8 /* && additional checks */);
-            }
-        });
-        lblVisiblePassword.textProperty().bind(pfPasswordProperty);
-    }
-
     //region initiate window
     private void loadWindow(Stage window) {
         window.setHeight(200);
         window.setWidth(300);
-        window.setTitle("Week 4 - Lesson 3 Exercise - Login Screen (Improved)");
+        window.setTitle("Login Screen (Improved)");
 
         gridPane.setPadding(new Insets(10, 10, 10, 10));
         gridPane.setVgap(10); // Vertical spacing between grid items
@@ -61,8 +51,6 @@ public class App extends Application {
         txtUsername.setPromptText("Enter username");
         pfPassword.setPromptText("Enter password");
 
-        btnLogin.getStyleClass().add("customButton");
-        btnLogin.setId("specialButton");
         btnLogin.setVisible(false);
     }
 
@@ -73,10 +61,31 @@ public class App extends Application {
         GridPane.setConstraints(txtUsername, 1, 0);
         GridPane.setConstraints(pfPassword, 1, 1);
         GridPane.setConstraints(btnLogin, 1, 2);
-        GridPane.setConstraints(lblVisiblePassword, 0, 4);
+        GridPane.setConstraints(lblVisiblePassword, 0, 4, 3, 1);
 
         gridPane.getChildren().addAll(lblUsername, lblPassword, txtUsername,
                 pfPassword, btnLogin, lblVisiblePassword);
     }
     //endregion
+
+    @Override
+    public void start(Stage window) throws Exception {
+        loadWindow(window);
+
+        StringProperty pfPasswordProperty = pfPassword.textProperty();
+        pfPasswordProperty.addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                btnLogin.setVisible(lblVisiblePassword.getText().matches("(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$"));
+            }
+        });
+        lblVisiblePassword.textProperty().bind(pfPasswordProperty);
+
+        btnLogin.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                showMessageDialog(null, "You are now successfully logged in");
+            }
+        });
+    }
 }
