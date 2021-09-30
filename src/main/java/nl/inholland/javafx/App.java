fe1e42190d732658ca1ecb4f56d2e56929a07d9e
REDitor <71357600+REDitor@users.noreply.github.com>
2021-09-30 21:08:03 +0200

    Horrible

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -7,38 +7,91 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.util.Random;
 
-import static javax.swing.JOptionPane.showMessageDialog;
-
 public class App extends Application {
-    Game game;
-    Player user;
-    Player computer;
+    Button[][] board = new Button[3][3];
+    int[] rows;
+    int[] columns;
+    int[] diagonal1;
+    int[] diagonal2;
 
     //region Elements
-    Button[][] buttons = new Button[3][3];
     Label lblCurrentTurn = new Label("Current turn:");
     Label lblCurrentTurnResult = new Label();
+
     GridPane gridPane = new GridPane();
     //endregion
 
-    //region InitiateWindow
-    private void loadWindow(Stage window) {
+    //region InitiateApp
+    private void createButtons() {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                board[row][col] = new Button("_");
+                board[row][col].setPrefSize(100, 100);
+                board[row][col].setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        lblCurrentTurnResult.setText("It's your turn!");
+
+                        //User turn
+                        Button clickedButton = (Button)actionEvent.getSource();
+                        clickedButton.setText("X");
+                        clickedButton.setDisable(true);
+                        if (checkForWinner()) {
+                            lblCurrentTurnResult.setText("Congratulations, you won the game!");
+                            return;
+                        }
+
+                        //Computer turn
+                        Random rng = new Random();
+                        int randomRow = rng.nextInt(3);
+                        int randomColumn = rng.nextInt(3);
+
+                        while (!checkTurn(randomRow, randomColumn)) {
+                            randomRow = rng.nextInt(3);
+                            randomColumn = rng.nextInt(3);
+                        }
+
+                        board[randomRow][randomColumn].setText("O");
+                        board[randomRow][randomColumn].setDisable(true);
+                        if (checkForWinner()) {
+                            lblCurrentTurnResult.setText("The computer won the game, better luck next time...");
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void assignGrid() {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                GridPane.setConstraints(board[row][col], col, row);
+                gridPane.getChildren().add(board[row][col]);
+            }
+        }
+
+        GridPane.setConstraints(lblCurrentTurn, 0, 3);
+        GridPane.setConstraints(lblCurrentTurnResult, 1, 3, 2, 1);
+        gridPane.getChildren().addAll(lblCurrentTurn, lblCurrentTurnResult);
+    }
+    //endregion
+
+    @Override
+    public void start(Stage window) throws Exception {
         window.setMinHeight(400);
         window.setMinWidth(340);
         window.setTitle("Tic-Tac-Toe");
 
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setPadding(new Insets(10));
         gridPane.setVgap(10);
         gridPane.setHgap(10);
 
-        CreateButtons();
-        styleNodes();
+        createButtons();
         assignGrid();
 
         Scene scene = new Scene(gridPane);
@@ -46,109 +99,33 @@ public class App extends Application {
         window.show();
     }
 
-    private void CreateButtons() {
-        for (int row = 0; row < buttons.length; row++) {
-            for (int col = 0; col < buttons[0].length; col++) {
-                buttons[row][col] = new Button("_");
-            }
-        }
-    }
-
-    private void assignGrid() {
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setPercentWidth(33);
-        ColumnConstraints col2 = new ColumnConstraints();
-        col2.setPercentWidth(33);
-        ColumnConstraints col3 = new ColumnConstraints();
-        col3.setPercentWidth(33);
-
-        gridPane.getColumnConstraints().addAll(col1, col2, col3);
-
-        for (int row = 0; row < buttons.length; row++) {
-            for (int col = 0; col < buttons[0].length; col++) {
-                GridPane.setConstraints(buttons[row][col], col, row);
-                gridPane.getChildren().add(buttons[row][col]);
-            }
-        }
-
-        GridPane.setConstraints(lblCurrentTurn, 0, 3, 3, 1);
-        GridPane.setConstraints(lblCurrentTurnResult, 1, 3);
-
-        gridPane.getChildren().addAll(lblCurrentTurn, lblCurrentTurnResult);
-    }
-
-    private void styleNodes() {
-        for (Button[] row : buttons) {
-            for (Button button : row) {
-                button.setPrefSize(100, 100);
-            }
-        }
-    }
-    //endregion
-
-    @Override
-    public void start(Stage window) throws Exception {
-        loadWindow(window);
-        startGame();
-
-        while (!checkForWinner()) {
-            for (Button[] row : buttons) {
-                for (Button button : row) {
-                    button.setOnAction(new EventHandler<ActionEvent>() {
-                        @Override
-                        public void handle(ActionEvent actionEvent) {
-                            int rowIndex = GridPane.getRowIndex(button);
-                            int columnIndex = GridPane.getColumnIndex(button);
-
-                            game.board = user.setTurn(rowIndex, columnIndex);
-                            button.setText(game.board[rowIndex][columnIndex]);
-                            button.setDisable(true);
-
-                            Random rng = new Random();
-
-                            while (game.board[rowIndex][columnIndex] != null) {
-                                rowIndex = rng.nextInt(game.board.length);
-                                columnIndex = rng.nextInt(game.board[0].length);
-                            }
-                            game.board = computer.setTurn(rowIndex, columnIndex);
-                            button.setText(game.board[rowIndex][columnIndex]);
-                            button.setDisable(true);
-                        }
-                    });
-
-                }
-            }
-        }
-        showMessageDialog(null, String.format("The %s won the game", game.winner.getClass()));
-    }
-
-    private void startGame() {
-        user = new User();
-        computer = new Computer();
-        game = Game.getInstance();
+    //region Logical Methods
+    private boolean checkTurn(int row, int column) {
+        return board[row][column].getText() == null;
     }
 
     private boolean checkForWinner() {
-        int userRow = 0;
-        int computerRow = 0;
 
-        for (int row = 0; row < game.board.length; row++) {
-            for (int col = 0; col < game.board[0].length; col++) {
-                if (userRow == 3) {
-                    game.winner = user;
+        //FIXME: crashes the application
+       int userScore = 0;
+       int computerScore = 0;
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board.length; col++) {
+                if (userScore == 3) {
                     return true;
-                }else if (computerRow == 3) {
-                    game.winner = computer;
+                } else if (computerScore == 3) {
                     return true;
                 }
 
-                if (game.board[row][col] == "X") {
-                    userRow++;
-                } else if (game.board[row][col] == "O") {
-                    computerRow++;
+                if (board[row][col].getText() == "X") {
+                    userScore++;
+                } else if (board[row][col].getText() == "O") {
+                    computerScore++;
                 }
             }
         }
         return false;
     }
+    //endregion
 }
--- src/main/java/nl/inholland/javafx/Player.java
+++
@@ -1,7 +0,0 @@
-package nl.inholland.javafx;
-
-public abstract class Player {
-    Game game = Game.getInstance();
-
-    abstract String[][] setTurn(int row, int column);
-}
