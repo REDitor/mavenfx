37d7db6aa23f53767f84c1bf0709b403b23f8d38
REDitor <71357600+REDitor@users.noreply.github.com>
2021-10-01 15:02:37 +0200

    Finished

--- src/main/java/nl/inholland/javafx/Database.java
+++ src/main/java/nl/inholland/javafx/Database.java
@@ -12,5 +12,8 @@ public class Database {
     public Database() {
         people.add(new Person("George", "Washington", LocalDate.of(1732, 2, 22)));
         people.add(new Person("John", "Adams", LocalDate.of(1735, 10, 30)));
+        people.add(new Person("Andre", "Harks", LocalDate.of(1963, 3, 26)));
+        people.add(new Person("Yvonne", "Bekema", LocalDate.of(1967, 4, 19)));
+        people.add(new Person("Daphne", "Harks", LocalDate.of(1994, 11, 4)));
     }
 }
