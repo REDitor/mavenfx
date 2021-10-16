24e3f2e08eed41606c6e43db805f75c0c1a2532a
REDitor <71357600+REDitor@users.noreply.github.com>
2021-10-16 18:22:46 +0200

    ...

--- src/main/java/nl/inholland/javafx/UI/App.java
+++ src/main/java/nl/inholland/javafx/UI/App.java
@@ -6,9 +6,14 @@ import nl.inholland.javafx.DataAccess.Database;
 
 public class App extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage window) {
         Database db = new Database();
         LoginWindow loginWindow = new LoginWindow(db);
+        window.close();
     }
 }
